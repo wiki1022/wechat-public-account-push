8796aea9db7f726cb195b2592ef40d570635de56
wiki1022 <115916142+wiki1022@users.noreply.github.com>
2022-10-16 20:21:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝子',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'okq-46L1j4ie31CZGoHxuy0mvEQA',
+      id: 'okq-46ITEm9J2XYIduy1zDTQr5SY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ZumUjGlBvAhp9cPgli3EtCFS7GM0Wjzw00HJx_C7E1U',
+      useTemplateId: 'vTqqnCqpakiZv9jsRRQhcPY1HY5qjskOif8xaojSGL4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-28',
       festivals: [
